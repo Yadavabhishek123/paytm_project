3bbb44bc895d91cf58e11b901c0e65cd2c9d848f
Yadavabhishek123 <abhishekyadav20je0043@gmail.com>
2024-06-07 09:42:47 +0530

    C&P

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,21 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import Signup from "./pages/Signup"
+import Signin from "./pages/Signin"
+import Dashboard from "./pages/Dashboard"
+import Sendmoney from "./pages/Sendmoney"
+
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<Signup/>}/>
+          <Route path="/signin" element={<Signin/>}/>
+          <Route path="/dashboard" element={<Dashboard/>}/>
+          <Route path="/send" element={<Sendmoney/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
